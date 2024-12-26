431682ed4ee9688553100edaae3c64a12fce3ea3
Adarsh <adarshkindrajith@gmail.com>
2024-12-26 21:13:39 +0530

    models updated

---
+++ project/product/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Product
+
+# Create your views here.
+def product(request):
+    products=Product.objects.all()
+    return render(request,'product/product.html',{'products':products})
--- project/project/settings.py
+++ project/project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home',
     'log',
+    'product',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL='media/'
+MEDIA_ROOT=os.path.join("BASE_DIR", 'media')
 
 
 STATICFILES_DIRS=[
