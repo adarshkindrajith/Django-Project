0d0537f1da624662e60938fb1297191ea37c09f6
Adarsh <adarshkindrajith@gmail.com>
2025-02-01 12:55:57 +0530

    new updates

--- project/product/admin.py
+++ project/product/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category,Customer,Order,Brand
+from .models import Category,Customer,Order,Brand,Product
 from .models import CarouselImage
 # Register your models here.
 admin.site.register(Category)
@@ -17,6 +17,12 @@ class CustomerModelAdmin(admin.ModelAdmin):
 @admin.register(Order)
 class OrderModelAdmin(admin.ModelAdmin):
     list_display=[ 'id','user','customer','product','quantity','date','phone','address','order_status','cancellation_requested','payment' ]
+
+
+@admin.register(Product)
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display=[ 'name','price','category','brand','description','image','is_sale','sale_price']
+
 
 @admin.register(CarouselImage)
 class CarouselImageAdmin(admin.ModelAdmin):
