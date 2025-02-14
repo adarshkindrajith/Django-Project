50a6703ac7f735dc48e997cbe01301d88a940540
Adarsh <adarshkindrajith@gmail.com>
2025-02-14 11:20:53 +0530

    updates

--- project/project/settings.py
+++ project/project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',cast=bool)
 
-ALLOWED_HOSTS = ['13.48.30.22', 'localhost', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['13.61.154.100', 'localhost', '127.0.0.1:8000']
 
 
 # Application definition
